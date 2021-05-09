b5a2e7227cafba4b1c72bb3de996fc9d025d748e
VanessaCarvalho-VNS <vnscarvalh@gmail.com>
2021-05-08 22:08:11 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,14 @@
+import NavBar from "components/NavBar";
+
 function App() {
   return (
-    <div >
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <>
+      <NavBar />
+      <div className="container">
+        <h1 className="text-primary">Olá mundo!</h1>
+        <NavBar />
+      </div>
+    </>
   );
 }
 
