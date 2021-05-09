86d3d85206631ee81050518c9261fee6dd869cf5
VanessaCarvalho-VNS <vnscarvalh@gmail.com>
2021-05-09 13:47:30 -0300

    Static Charts

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -12,11 +12,11 @@ function App() {
         <h1 className="text-primary py-3">Dashboard de Vendas</h1>
 
         <div className="row px-3">
-          <div className="col-sn-6">
+          <div className="col-sm-6">
             <h5 className="text-center text-secondary">Taxa de sucesso(%)</h5>
             <BarChart />
           </div>
-          <div className="col-sn-6">
+          <div className="col-sm-6">
             <h5 className="text-center text-secondary">Todas vendas</h5>
             <DonutChart />
           </div>
