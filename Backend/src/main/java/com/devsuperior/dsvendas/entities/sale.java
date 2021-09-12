c53dff95693da90751e79352b680758de6ec6586
Vanessa Carvalho <vnscarvalh@gmail.com>
2021-09-11 22:19:48 -0300

    Domain model, database seed

--- Backend/src/main/java/com/devsuperior/dsvendas/entities/sale.java
+++ Backend/src/main/java/com/devsuperior/dsvendas/entities/sale.java
@@ -3,16 +3,27 @@ package com.devsuperior.dsvendas.entities;
 import java.time.LocalDate;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_sales")
 public class sale {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	private Integer visited;
 	private Integer deals;
 	private Double amount;
 	private LocalDate date;
 	
+	@ManyToOne
+	@JoinColumn(name = "seller_id")
 	private seller seller;
 	
 	public sale() {	
--- Backend/src/main/java/com/devsuperior/dsvendas/entities/seller.java
+++ Backend/src/main/java/com/devsuperior/dsvendas/entities/seller.java
@@ -3,11 +3,23 @@ package com.devsuperior.dsvendas.entities;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sellers")
 public class seller {
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	
+	@OneToMany(mappedBy = "seller")
 	private List<sale> sales = new 	ArrayList<>();
 	
 	public seller() {
